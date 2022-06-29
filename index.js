e856268b863e4a06e6577d1138f8ae5339d9c308
Nievanik <108207961+Nievanik@users.noreply.github.com>
2022-06-29 22:07:07 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log("Hello");
+function CV(){
+    alert("CV is not available rignt now");
+}
+
+let title =document.querySelector(".title")
+let a = document.querySelector(`.hamburger`)
+a.addEventListener("click", ()=>{
+    title.classList.toggle('navappear')
+});
+
+
+// function ham_click(){
+//     console.log("Hello");
+// }
